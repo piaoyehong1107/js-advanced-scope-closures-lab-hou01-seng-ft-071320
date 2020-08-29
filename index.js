f85bed2eab960a3bea034ac0a8c172653d10e4a1
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-29 05:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function produceDrivingRange(range)
  return function (str1, str2){
    if (parseInt(str2)-parseInt(str1) < range){
      return `within range by ${range-(parseInt(str2)-parseInt(str1))}`
+     else if (parseInt(str2)-parseInt(str1) > range){
+     return ` ${range-(parseInt(str2)-parseInt(str1))} `
    }
     
  }
