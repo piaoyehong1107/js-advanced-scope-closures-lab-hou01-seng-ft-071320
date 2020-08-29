e02de1b7612b8100dd26205940cb1fa675355538
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-29 05:31:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ function produceDrivingRange(range){
    if (parseInt(str2)-parseInt(str1) < range){
      return `within range by ${range-(parseInt(str2)-parseInt(str1))}`
      
-   }
+      }
      else if (parseInt(str2)-parseInt(str1) > range){
      return ` ${(parseInt(str2)-parseInt(str1))-range} blocks out of range`
-   }
+      }
+    }
 }
-}}
+}
