6586ea790df98e58559d6a20381c25b1f06af5a2
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-29 05:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function produceDrivingRange(range)
+function produceDrivingRange(range){
  return function (str1, str2){
    if (parseInt(str2)-parseInt(str1) < range){
      return `within range by ${range-(parseInt(str2)-parseInt(str1))}`
@@ -8,4 +8,4 @@ function produceDrivingRange(range)
      return ` ${(parseInt(str2)-parseInt(str1))-range} blocks out of range`
    }
 }
-}
+}}
